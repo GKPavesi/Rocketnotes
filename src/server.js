da0c42301f7282399a7b7a0e7f1f8eaf28830d70
GKPavesi <guipavesi@hotmail.com>
2023-03-12 01:08:03 -0300

    error handling added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
+require('express-async-errors');
+
+const AppError = require('./utils/AppError');
 const express = require("express");
-const routes = require('./routes')
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json()); //// habilita nosso app a entender json no body das requests
 app.use(routes); /// utiliza as routes que disponibilizamos na pasta routes
+app.use(( error, request, response, next ) => {    ///esse trem aqui é para tratar erro tanto de cliente quanto do server
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
-})
+});
