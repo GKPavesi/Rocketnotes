447dd633d8545abf2393ddb3f3fe920fbb75e76d
GKPavesi <guipavesi@hotmail.com>
2023-03-27 21:37:09 -0300

    new funcionalities in NotesController and creation of TagsController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -33,7 +33,7 @@ class NotesController {
         return response.status(201).json();
     }
 
-    async show(request, response) {
+    async show(request, response) { //puxando nota especifica
         const { id } = request.params;
 
         const note = await knex("notes").where({ id }).first();
@@ -53,6 +53,46 @@ class NotesController {
         await knex("notes").where({ id }).delete();
 
         return response.json();
+    }
+
+    async index(request, response) { //puxando notas por filtros, tais como usuario
+        const { title, user_id, tags } = request.query;
+
+        let notes;
+
+        if (tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+            
+            notes = await knex("tags")
+            .select([
+                "notes.id",
+                "notes.title",
+                "notes.user_id"
+            ])
+            .where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
+            .whereIn("name", filterTags)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
+
+        } else {
+            notes = await knex("notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy("title");
+        }
+
+        const userTags = await knex("tags").where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+
+        return response.json(notesWithTags)
     }
 }
 
